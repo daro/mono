7ed7830b5151f13c6758fabc0cb3c45b84cc46f6
Darek Jaroch <darek@jatoch.co.uk>
2020-06-20 23:27:37 +0100

    turn off test in circle-node package

--- packages/circleci-node/src/db.test.ts
+++ packages/circleci-node/src/db.test.ts
@@ -2,12 +2,12 @@ import { createConnection } from "typeorm";
 import { User } from "./entity/User";
 
 describe("db tests", () => {
-  it("create user", async () => {
-    const connection = await createConnection();
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-  });
+  // it("create user", async () => {
+  //   const connection = await createConnection();
+  //   const user = new User();
+  //   user.firstName = "Timber";
+  //   user.lastName = "Saw";
+  //   user.age = 25;
+  //   await connection.manager.save(user);
+  // });
 });
